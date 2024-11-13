adb8fb708240541f67d793bc2814367641d30abb
Ajay SIngh 22 <iamajaysingh22@gmail.com>
2024-11-13 15:57:50 +0530

    add auth configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 // import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos, setTodos] = useState<string[]>([]);
 
   useEffect(() => {
     setTodos(['todo1', 'todo2', 'todo3']);
@@ -15,7 +16,7 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+    // client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
   return (
@@ -23,9 +24,9 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
+        {/* {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
-        ))}
+        ))} */}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo. 4232352
